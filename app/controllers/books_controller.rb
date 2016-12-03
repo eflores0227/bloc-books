7311478d3e1ef26679386986e8893907e6f65370
Estefania Flores <eflores0227@gmail.com>
2016-12-02 19:36:06 -0800

    assignment 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,13 @@ class BooksController < BlocWorks::Controller
   def welcome
     render :welcome, book: "Eloquent Ruby"
   end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
